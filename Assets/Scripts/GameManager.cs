309f944e541820c8483e74bfe79e53efc58e39ed
João Victor Moreira <joaovictorfersuramoreira@gmail.com>
2021-12-07 22:58:37 -0300

    Singleton Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            //create logic to create the instance
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("Game Manager");
+                go.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public int Score { get; set; }
+
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        Score = 10;
+    }
+
+}
