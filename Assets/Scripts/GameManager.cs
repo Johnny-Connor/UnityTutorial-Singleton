5bea8636ebdb4c6ccca55ea22d0355ea2132ccd9
João Victor Moreira <joaovictorfersuramoreira@gmail.com>
2021-12-13 17:27:05 -0300

    Fixed a bug
    4 days ago Github said I had to pull some data into my project. It turns out that was some very old piece of code that prevented the game from starting.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        if (Instance == null)
+        if (_instance == null)
         {
             _instance = this;
             DontDestroyOnLoad(gameObject);
