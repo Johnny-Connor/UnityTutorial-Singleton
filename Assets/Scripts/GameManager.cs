15a2872bb8086799269e7216815a158519c6a192
João Victor Moreira <joaovictorfersuramoreira@gmail.com>
2021-12-14 22:59:49 -0300

    Code comments completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,18 +2,25 @@
 
 public class GameManager : MonoBehaviour
 {
-    // This variable is responsible for storing a instance of the GameManager class. In our algorithm, if it returns null, it'll mean that the class hasn't been instantiated yet.
+
+    /* This variable is responsible for storing a instance of the GameManager class. 
+     * In our algorithm, if it returns null, it'll mean that the class hasn't been instantiated yet. */
     private static GameManager _instance;
 
-    // Here lies the get property. Any script wanting to access our GameManager class will have to mention this property to do so.
+    /* Here lies the get property. Any script wanting to access our GameManager class will have to
+     * mention this property to do so. */
     public static GameManager Instance
     {
         get
         {
-            // To be commented...
+            /* This condition happens only once during the execution of the game. That's because
+             * "_instance" gets a value as soon as a GameObject with this script is created (take
+             * a look at the "Awake" method). */
             if (_instance == null)
             {
+                // Creating a GameObject called "GameManager".
                 GameObject go = new GameObject("GameManager");
+                // Adding this script inside it.
                 go.AddComponent<GameManager>();
             }
             return _instance;
@@ -28,20 +35,22 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        // Again, this condition will happen only once throughout the execution of the game.
         if (_instance == null)
         {
+            // Quick reminder: "this" refers to the current instance of the class.
             _instance = this;
+            /* Prevents the GameObject "GameManager" from getting deleted during scene transitions.
+             * It's important to do this because the GameObject we created is holding this script
+             * as a component. Letting it be destroyed would result in the loss of the non static
+             * variables values (such as "Money", for instance). */
             DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
         }
     }
 
     private void Start()
     {
-        //You can give initial values to your GameManager's variables in the "Start" function.
+        // You can give initial values to your GameManager's variables in the "Start" method.
         Money = 3;
     }
 
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,6 +10,10 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        /* You might be wondering: how can "GameManager.cs" work without it being in the Hierarchy? The reason for that's because
+         * every other script in the assets folder actually functions outside the Hierarchy too. We normally don't notice that 
+         * because it's impossible to use non static methods/functions that aren't in the Hierarchy. You can comprove it by yourself
+         * by making either "Instance" or "_instance" non static and trying to run the game. */
         MoneyText.text = "Money: $" + GameManager.Instance.Money.ToString("n0", CultureInfo.InvariantCulture);
         CoffeesOwnedText.text = "Coffees Owned: " + GameManager.Instance.CoffeesOwned.ToString("n0", CultureInfo.InvariantCulture);
         PhonesOwnedText.text = "Phones Owned: " + GameManager.Instance.PhonesOwned.ToString("n0", CultureInfo.InvariantCulture);
